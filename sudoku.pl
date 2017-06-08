bec65e10d7103cdf93789c331401ffe9cef026a8
Jeremy <jgmiller88@gmail.com>
2017-06-07 22:52:06 -0700

    beginning of sudoku solver

---
+++ sudoku.pl
@@ -0,0 +1,14 @@
+sudoku(Puzzle, Solution) :-
+  Solution = Puzzle,  % goal is to have our Solution equal our Puzzle
+  N is sqrt(length(Puzzle)),  % determine size of grid (NxN)
+  fd_domain(Puzzle, 1, N).  % constrain all values in Puzzle to valid values
+  % setup rows
+  % setup columns
+  % setup squares
+  % validate rows
+  % validate columns
+  % validate squares
+  % validate fd_all_different()
+  
+  % write tests
+  % get working in Docker image
