9c6ea2eb6eeeeba6b0a2c1ca18af8a243b929b86
Jeremy <jgmiller88@gmail.com>
2017-06-11 20:33:21 -0700

    add print functions

--- sudoku.pl
+++ sudoku.pl
@@ -9,14 +9,27 @@ sudoku(Rows) :-                         % expects list of lists
   maplist(all_distinct, Columns),       % make sure all column values are distinct
   Block_Width is ceiling(sqrt(Size)),
   Block_Height is floor(sqrt(Size)),
+  validate_blocks(Rows),
   % for each row in Block_Height get Block_Width elements and maplist(all_distinct, Block)
   % generate blocks
   % validate blocks
   maplist(label, Rows),                 % make sure all cells have only one value
-  % pretty print output
+  print_solution(Rows).
 
-take(Block_Height, Rows, Block_Rows)
-take(Block_Width, Block_Rows, )
+validate_blocks(Rows) :-
+  take(Block_Height, Rows, Block_Rows)
+  take(Block_Width, Block_Rows, )
 
+print_solution([]).
+print_solution([H|T]) :-
+  print_row(H),
+  write('|'), nl,
+  print_solution(T).
 
-  % get working in Docker image
+print_row([]).
+print_row([H|T]) :-
+  write('|').
+  write(H),
+  print_row(T).
+
+  % get working in Docker image - https://hub.docker.com/r/swipl/swipl/
