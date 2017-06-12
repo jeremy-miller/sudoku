8312f5785aea6edeb7c687acff24bb54864379af
Jeremy <jgmiller88@gmail.com>
2017-06-11 23:17:20 -0700

    first pass at solver

--- sudoku.pl
+++ sudoku.pl
@@ -1,35 +1,46 @@
 :- use_module(library(clpfd)).
 
 sudoku(Rows) :-                         % expects list of lists
-  Size is sqrt(length(flatten(Rows))),  % determine size of input puzzle
+  flatten(Rows, Flattened_Rows),
+  length(Flattened_Rows, Flattened_Rows_Length),
+  Size is sqrt(Flattened_Rows_Length),  % determine size of input puzzle
   Domain ins 1..Size,
   append(Rows, Domain),                 % make sure all values in rows contain valid values
   maplist(all_distinct, Rows),          % make sure all row values are distinct
   transpose(Rows, Columns),             % generate all columns from exisitng rows
   maplist(all_distinct, Columns),       % make sure all column values are distinct
-  Block_Width is ceiling(sqrt(Size)),
-  Block_Height is floor(sqrt(Size)),
-  validate_blocks(Rows),
-  % for each row in Block_Height get Block_Width elements and maplist(all_distinct, Block)
-  % generate blocks
-  % validate blocks
+  get_blocks(Size, Rows, Blocks),
+  maplist(all_distinct, Blocks),        % make sure all blocks are distinct
   maplist(label, Rows),                 % make sure all cells have only one value
   print_solution(Rows).
 
-validate_blocks(Rows) :-
-  take(Block_Height, Rows, Block_Rows)
-  take(Block_Width, Block_Rows, )
+get_blocks(_, [], _).
+get_blocks(Size, Rows, Blocks) :-
+  Block_Height is floor(sqrt(Size)),
+  Block_Width is ceiling(sqrt(Size)),
+  Block_Length is Block_Height * Block_Width,
+  split_at(Block_Height, Rows, First_Rows, Rest_Rows),  % get first set of rows to get blocks from
+  transpose(First_Rows, Block_Columns),  % setup columns to easily pull out blocks from first set of rows
+  flatten(Block_Columns, Flattened_Blocks),  % flatten columns (now that blocks are all next to each other)
+  partition(Flattened_Blocks, Block_Length, Row_Blocks),  % get the blocks from this set of rows
+  append(Row_Blocks, Blocks),  % add blocks to existing list of blocks
+  get_blocks(Size, Rest_Rows, Blocks).
+
+partition(_, [], []).
+partition(Length, List, [First_Block|Rest_Blocks]) :-
+  take(Length, List, First_Block, Rest),
+  partition(Length, Rest, Rest_Blocks).
 
 print_solution([]).
-print_solution([H|T]) :-
-  print_row(H),
+print_solution([Head|Tail]) :-
+  print_row(Head),
   write('|'), nl,
-  print_solution(T).
+  print_solution(Tail).
 
 print_row([]).
-print_row([H|T]) :-
-  write('|').
-  write(H),
-  print_row(T).
+print_row([Head|Tail]) :-
+  write('|'),
+  write(Head),
+  print_row(Tail).
 
   % get working in Docker image - https://hub.docker.com/r/swipl/swipl/
