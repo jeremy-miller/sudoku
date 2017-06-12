69321c195ad29f94b42e14fa0a76444b7c591fb1
Jeremy <jgmiller88@gmail.com>
2017-06-11 18:26:51 -0700

    sudoku solver WIP, add tests

--- sudoku.pl
+++ sudoku.pl
@@ -7,39 +7,16 @@ sudoku(Rows) :-                         % expects list of lists
   maplist(all_distinct, Rows),          % make sure all row values are distinct
   transpose(Rows, Columns),             % generate all columns from exisitng rows
   maplist(all_distinct, Columns),       % make sure all column values are distinct
+  Block_Width is ceiling(sqrt(Size)),
+  Block_Height is floor(sqrt(Size)),
+  % for each row in Block_Height get Block_Width elements and maplist(all_distinct, Block)
   % generate blocks
   % validate blocks
   maplist(label, Rows),                 % make sure all cells have only one value
   % pretty print output
 
-
-  Block_Width is ceiling(sqrt(Size)),
-  Block_Height is floor(sqrt(Size)),
-
-
-[[_,_,2,3],
- [_,_,_,_],
- [_,_,_,_],
- [3,4,_,_]]
-
- [[1,_,_,_,_,_],
-  [_,5,_,2,_,_],
-  [_,1,6,_,5,_], 
-  [_,3,_,6,2,_], 
-  [_,_,1,_,3,_], 
-  [_,_,_,_,_,5]]
+take(Block_Height, Rows, Block_Rows)
+take(Block_Width, Block_Rows, )
 
 
-[[_,_,_,_,_,_,_,_,_],
- [_,_,_,_,_,3,_,8,5],
- [_,_,1,_,2,_,_,_,_],
- [_,_,_,5,_,7,_,_,_],
- [_,_,4,_,_,_,1,_,_],
- [_,9,_,_,_,_,_,_,_],
- [5,_,_,_,_,_,_,7,3],
- [_,_,2,_,1,_,_,_,_],
- [_,_,_,_,4,_,_,_,9]]
-
-
-  % write tests
   % get working in Docker image
