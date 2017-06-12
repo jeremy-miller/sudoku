d3e32f4d5fcc70da77cc60928ce72d4f2e3f4cfb
Jeremy <jgmiller88@gmail.com>
2017-06-12 07:32:27 -0700

    cleanup block validation

--- sudoku.pl
+++ sudoku.pl
@@ -1,35 +1,34 @@
 :- use_module(library(clpfd)).
 
-sudoku(Rows) :-                         % expects list of lists
-  flatten(Rows, Flattened_Rows),
-  length(Flattened_Rows, Flattened_Rows_Length),
-  Size is sqrt(Flattened_Rows_Length),  % determine size of input puzzle
+sudoku(Rows) :-                     % expects list of lists
+  Size is length(Rows),             % determine size of input puzzle
   Domain ins 1..Size,
-  append(Rows, Domain),                 % make sure all values in rows contain valid values
-  maplist(all_distinct, Rows),          % make sure all row values are distinct
-  transpose(Rows, Columns),             % generate all columns from exisitng rows
-  maplist(all_distinct, Columns),       % make sure all column values are distinct
-  get_blocks(Size, Rows, Blocks),
-  maplist(all_distinct, Blocks),        % make sure all blocks are distinct
-  maplist(label, Rows),                 % make sure all cells have only one value
+  append(Rows, Domain),             % make sure all values in rows contain valid values
+  maplist(all_distinct, Rows),      % make sure all row values are distinct
+  transpose(Rows, Columns),         % generate all columns from exisitng rows
+  maplist(all_distinct, Columns),   % make sure all column values are distinct
+  validate_blocks(Size, Rows),      % make sure all blocks contain valid values and are distinct
+  maplist(label, Rows),             % make sure all cells have only one value
   print_solution(Rows).
 
-get_blocks(_, [], _).
-get_blocks(Size, Rows, Blocks) :-
+validate_blocks(_, []).
+validate_blocks(Size, Rows) :-
+  Domain ins 1..Size,
   Block_Height is floor(sqrt(Size)),
   Block_Width is ceiling(sqrt(Size)),
   Block_Length is Block_Height * Block_Width,
-  split_at(Block_Height, Rows, First_Rows, Rest_Rows),  % get first set of rows to get blocks from
-  transpose(First_Rows, Block_Columns),  % setup columns to easily pull out blocks from first set of rows
-  flatten(Block_Columns, Flattened_Blocks),  % flatten columns (now that blocks are all next to each other)
-  partition(Flattened_Blocks, Block_Length, Row_Blocks),  % get the blocks from this set of rows
-  append(Row_Blocks, Blocks),  % add blocks to existing list of blocks
-  get_blocks(Size, Rest_Rows, Blocks).
+  split_at(Block_Height, Rows, First_Rows, Rest_Rows),          % get first set of rows to get blocks from
+  transpose(First_Rows, Block_Columns),                         % setup columns to easily pull out blocks from first set of rows
+  flatten(Block_Columns, Flattened_Blocks),                     % flatten columns (now that blocks are all next to each other)
+  validate_row_blocks(Domain, Block_Length, Flattened_Blocks),  % validate the blocks from this set of rows
+  validate_blocks(Size, Rest_Rows).
 
-partition(_, [], []).
-partition(Length, List, [First_Block|Rest_Blocks]) :-
-  take(Length, List, First_Block, Rest),
-  partition(Length, Rest, Rest_Blocks).
+validate_row_blocks(_, _, []).
+validate_row_blocks(Domain, Block_Length, List) :-
+  take(Block_Length, List, First_Block, Rest),      % get first block from this set of rows
+  append(First_Block, Domain),                      % make sure all cells in block contain valid values
+  all_distinct(First_Block),                        % make sure all cells in block are distinct
+  validate_row_blocks(Domain, Block_Length, Rest).  % recursively validate rest of blocks in this set of rows
 
 print_solution([]).
 print_solution([Head|Tail]) :-
